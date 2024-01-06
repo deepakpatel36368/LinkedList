e787502467beeb944f2ee297ea83b3882a407d31
deepak <deepkpatel36368@gmail.com>
2024-01-06 13:01:59 +0530

    Create PR

--- linkedListCoad/src/main/java/org/example/Main.java
+++ linkedListCoad/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.SingleLinkList.NodeOperation;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("********* Linked List Operations *********");
